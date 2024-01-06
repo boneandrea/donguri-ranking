6876cb197e51a22322b62a7e8dfa794e8a9bb13a
Ubuntu <ubuntu@ip-172-26-7-226.ap-northeast-1.compute.internal>
2024-01-06 12:29:06 +0900

    setup prettier

--- manage.js
+++ manage.js
@@ -1,37 +1,31 @@
-window.addEventListener("input", e=>{
-    const self=e.srcElement
-    if(!self.classList.contains("score")) return
+window.addEventListener('input', (e) => {
+  const self = e.srcElement
+  if (!self.classList.contains('score')) return
 
-    const span= self
-          .parentNode
-          .parentNode
-          .querySelector(".changed")
-    if(self.value === "")
-        span.classList.add("hide")
-    else
-        span.classList.remove("hide")
+  const span = self.parentNode.parentNode.querySelector('.changed')
+  if (self.value === '') span.classList.add('hide')
+  else span.classList.remove('hide')
 })
 
-document.querySelector("button").addEventListener("click", ()=>{
-    if(confirm("reset??")){
-        window.location.reload()
-    }
+document.querySelector('button').addEventListener('click', () => {
+  if (confirm('reset??')) {
+    window.location.reload()
+  }
 })
 
-document.querySelector("form").addEventListener("submit", e=>{
-    const gross=qaa("input[type=number]")
-    let noinput=true;
-    gross.forEach(e=>{
-        if(e.value !== "")
-            noinput=false;
-    })
-    if(noinput){
-        e.preventDefault()
-        alert("input score")
-        return
-    }
+document.querySelector('form').addEventListener('submit', (e) => {
+  const gross = qaa('input[type=number]')
+  let noinput = true
+  gross.forEach((e) => {
+    if (e.value !== '') noinput = false
+  })
+  if (noinput) {
+    e.preventDefault()
+    alert('input score')
+    return
+  }
 
-    if(!confirm("登録OK?")){
-        e.preventDefault()
-    }
+  if (!confirm('登録OK?')) {
+    e.preventDefault()
+  }
 })
