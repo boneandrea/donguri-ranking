095d12345e426cd564520301680c6d541bb0df17
Ubuntu <ubuntu@ip-172-26-7-226.ap-northeast-1.compute.internal>
2024-01-15 18:51:55 +0900

    Restore last data

--- score/index.js
+++ score/index.js
@@ -28,16 +28,16 @@ q('#score').addEventListener('click', (e) => {
   if (!e.target.classList.contains('btn')) return
   const button = e.target
   const tr = button.closest('tr')
-  const hole = getHoleAndName(tr,button)
+  const hole = getHoleAndName(tr, button)
   q('#hole').innerText = hole
   myModal.show()
   currentButton = e.target
 })
 
-const getHoleAndName=(tr,button)=>{
-    const hole = qaa('button', tr).indexOf(button) + 1
-    const name = q('th', tr).innerText
-    return `${hole}: player ${name}`
+const getHoleAndName = (tr, button) => {
+  const hole = qaa('button', tr).indexOf(button) + 1
+  const name = q('th', tr).innerText
+  return `${hole}: player ${name}`
 }
 
 // スコア入力
@@ -56,21 +56,27 @@ q('#myModal').addEventListener('click', (e) => {
 // 全部クリア
 q('#clear').addEventListener('click', () => {
   if (!confirm('Clear all??')) return
+
   qaa('td button', q('tbody')).forEach((e) => (e.innerHTML = '-'))
   update()
+  clearData()
 })
 
 q('h1 span').innerText = `(${new Date().toLocaleDateString('ja-JP')})`
 
 const update = () => {
   const trs = qaa('tbody tr')
-  trs.forEach((tr) => {
+  const data = []
+  trs.forEach((tr, tr_index) => {
+    data[tr_index] = []
     const tds = qaa('td button', tr)
     const total1 = tds
       .slice(0, 9)
       .map((td) => {
-        const score = parseInt(td.innerHTML)
-        return isNaN(score) ? 0 : score
+        const _score = parseInt(td.innerHTML)
+        const score = isNaN(_score) ? 0 : _score
+        data[tr_index].push(score)
+        return score
       })
       .reduce((sum, score) => sum + score)
     q('.total1', tr).innerText = total1
@@ -78,12 +84,42 @@ const update = () => {
     const total2 = tds
       .slice(9, 18)
       .map((td) => {
-        const score = parseInt(td.innerHTML)
-        return isNaN(score) ? 0 : score
+        const _score = parseInt(td.innerHTML)
+        const score = isNaN(_score) ? 0 : _score
+        data[tr_index].push(score)
+        return score
       })
       .reduce((sum, score) => sum + score)
 
     q('.total2', tr).innerText = total2
     q('.total3', tr).innerText = total1 + total2
   })
+  storeData(data)
 }
+
+const restoreData = () => {
+  const lastData = localStorage.getItem('score')
+  if (lastData) {
+    console.log('restore editing data')
+    const parsed = JSON.parse(lastData)
+
+    const trs = qaa('tbody tr')
+    parsed.forEach((userData, tr_index) => {
+      const button = qaa('td button', trs[tr_index])
+      userData.forEach((score, hole_index) => {
+        button[hole_index].innerText = score ? score : '-'
+      })
+    })
+    update()
+  }
+}
+
+const storeData = (data) => {
+  localStorage.setItem('score', JSON.stringify(data))
+}
+
+const clearData = () => {
+  localStorage.removeItem('score')
+}
+
+restoreData()
